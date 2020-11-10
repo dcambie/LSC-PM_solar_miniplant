b9c6e52e85c12fa2220532b42adc54df26f5dc6e
Dario Cambié <dario.cambie@mpikg.mpg.de>
2020-11-10 22:36:04 +0100

    Implement run_simulation in basic_reactor_simulation.py

--- basic_reactor_simulation.py
+++ basic_reactor_simulation.py
@@ -15,27 +15,24 @@ TOTAL_PHOTONS = 100
 RENDER = False
 
 
-def run_simulation(location, time, num_photons: int = 100, render: bool = False) -> float:
-    
-    scene = create_standard_scene(tilt_angle=20, solar_elevation=40, solar_azimuth=180)
+def run_simulation(tilt_angle: int = 0, solar_elevation: int = 30, solar_azimuth: int = 180,
+                   num_photons: int = 100, render: bool = False) -> float:
+    # Create scene with the provided parameters
+    scene = create_standard_scene(tilt_angle=tilt_angle, solar_elevation=solar_elevation, solar_azimuth=solar_azimuth)
 
-    if RENDER:
+    if render:
         renderer = MeshcatRenderer(wireframe=False, open_browser=False)
         renderer.render(scene)
     finals = []
-    count = 0
 
-    for ray in scene.emit(TOTAL_PHOTONS):
-        count += 1
+    for ray in scene.emit(num_photons):
         steps = photon_tracer.follow(scene, ray)
         path, events = zip(*steps)
         finals.append(events[-1])
-        if RENDER:
+        if render:
             renderer.add_ray_path(path)
-        if count % 100 == 0:
-            print(count)
 
     count_events = collections.Counter(finals)
-    efficiency = count_events[Event.REACT] / TOTAL_PHOTONS
+    efficiency = count_events[Event.REACT] / num_photons
     LOGGER.info(f"Efficiency is {efficiency:.3f}")
     return efficiency
