b87cf23149f01dcb3efc9b2e04ce6989373e02bf
Stefan Zondag <stefan-zondag@hotmail.com>
2021-01-05 09:49:52 +0100

    Added surface fraction to yearly simulation and folder for simulation results.

--- yearly_simulation.py
+++ yearly_simulation.py
@@ -6,13 +6,19 @@ import logging
 import time
 
 from pvlib.location import Location
+from pvtrace.material.utils import spherical_to_cart
 
 from basic_reactor_simulation import run_simulation
 from solar_data import solar_data_for_place_and_time
 
 LOCATION = Location(51.4416, 5.6497, 'Europe/Amsterdam', 17, 'Eindhoven')
-TIME_RANGE = pd.date_range(start=datetime.datetime(2020, 1, 1), end=datetime.datetime(2021, 1, 1), freq='2H')
+TIME_RANGE = pd.date_range(start=datetime.datetime(2020, 1, 1), end=datetime.datetime(2021, 1, 1), freq='0.5H')
 
+def surface_incident(tilt_angle: int = 30, solar_elevation: int = 30, solar_azimuth: int = 180):
+    reactor_normal = spherical_to_cart(np.radians(tilt_angle), 0)
+    solar_light_normal = spherical_to_cart(np.radians(-solar_elevation + 90), np.radians(-solar_azimuth + 180))
+    surface_fraction = np.dot(reactor_normal, solar_light_normal)
+    return surface_fraction
 
 def evaluate_tilt_angle(tilt_angle: int):
     logger = logging.getLogger("pvtrace").getChild("miniplant")
@@ -35,7 +41,8 @@ def evaluate_tilt_angle(tilt_angle: int):
                                           solar_elevation=df['apparent_elevation'],
                                           solar_spectrum_function=photon_factory, num_photons=100)
         logger.info(f"Simulation completed for {df.name}")
-        df['efficiency_corrected'] = df['efficiency'] * df['ghi']
+        df['surface_fraction'] = surface_incident(tilt_angle, df['apparent_elevation'], df['azimuth'])
+        df['efficiency_corrected'] = df['efficiency'] * df['ghi'] * df['surface_fraction']
         return df
 
     start = time.time()
@@ -46,7 +53,7 @@ def evaluate_tilt_angle(tilt_angle: int):
     print(f"Year sum for this is {year_sum}")
 
     # These now include efficiency and efficiency_corrected
-    results.to_csv(f"raw_results/{LOCATION.name}_{tilt_angle}deg_results.csv",
+    results.to_csv(f"paper_results/{LOCATION.name}_{tilt_angle}deg_results.csv",
                    columns=("apparent_elevation", "azimuth", "ghi", "dhi", "efficiency", 'efficiency_corrected'))
 
 
