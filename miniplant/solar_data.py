52cd741db7a51776a2ac7743a946afe311feabed
Dario Cambié <dario.cambie@glasgow.ac.uk>
2021-02-01 12:27:39 +0100

    Rewrite solar data to make better use of pvlib data
    From spectral2 we already get irradiance on tilted surface so there is no point
    in getting the dhi/dni and then re-calculate something we already have ;)

--- miniplant/solar_data.py
+++ miniplant/solar_data.py
@@ -43,38 +43,48 @@ def solar_data_for_place_and_time(site: Location, datetime_points: pd.core.index
     # Solar position
     sol_pos: pd.DataFrame = site.get_solarposition(times=local_time)
 
-    # Clear sky irradiance replaced with integral over spctral2 ROI
-    clearsky_irradiance = site.get_clearsky(times=local_time, solar_position=sol_pos)
-
     # Relative Air Mass
     relative_airmass: pd.DataFrame = site.get_airmass(times=local_time, solar_position=sol_pos)
-    solar_data = pd.concat([sol_pos, relative_airmass, clearsky_irradiance], axis=1)
+    solar_data = pd.concat([sol_pos, relative_airmass], axis=1)
     # print(solar_data.columns)
     # ['apparent_zenith', 'zenith', 'apparent_elevation', 'elevation',
-    #        'azimuth', 'equation_of_time', 'airmass_relative', 'airmass_absolute',
-    #        'ghi', 'dni', 'dhi']
+    #        'azimuth', 'equation_of_time', 'airmass_relative', 'airmass_absolute']
 
     def calculate_spectrum(df):
         """ Calculate diffuse and direct spectra for every time point at the given location and tilt angle """
         df['aoi'] = irradiance.aoi(surface_tilt=tilt_angle, surface_azimuth=180, solar_zenith=df["apparent_zenith"],
                                    solar_azimuth=df["azimuth"])
 
+        # Use SPCTRAL2 model for irradiance on tilted surface
         solar_spectrum = spectrum.spectrl2(apparent_zenith=df["apparent_zenith"], aoi=df['aoi'],
                                            surface_tilt=tilt_angle, ground_albedo=albedo, surface_pressure=pressure,
                                            relative_airmass=df['airmass_relative'],
                                            precipitable_water=water_vapor_content, ozone=ozone,
                                            aerosol_turbidity_500nm=tau500, dayofyear=df.name.dayofyear)
 
-        # Add spectra to dataframe (trimmed to UV-VIS) [in particular 10-37 is 360--690 nm]
-        df['direct_spectrum'] = Distribution(solar_spectrum['wavelength'][10:37], solar_spectrum['dni'][10:37])
-        df['diffuse_spectrum'] = Distribution(solar_spectrum['wavelength'][10:37],
-                                              solar_spectrum['poa_sky_diffuse'][10:37])
+        """
+        PoA (plane of array) values already correct for tilt angle. This means the following:
+        
+        * Direct component is essentially DNI * pvlib.irradiance.aoi_projection() [that is dot product of solar vector
+            and surface normal. If the sun is behind the array the result is negative, we will set that to 0 see below.
+        * Diffuse component is the dhi corrected with the Hay & Davies 1980 model for sky diffuse component
+        
+        NOTE: we are neglecting the ground diffuse component here! (I_tilt = I_beam + I_sky + I_ground)
+        """
 
-        x = solar_spectrum['wavelength']
-        y = np.squeeze(solar_spectrum['dni'])
-        y2 = np.squeeze(solar_spectrum['dhi'])
-        df["dni_spctral"] = integrate.trapz(y, x)
-        df["dhi_spctral"] = integrate.trapz(y2, x)
+        # Add solar spectra for diffuse and direct to dataframe (trimmed to UV-VIS) [in particular 10-37 is 360--690 nm]
+        try:
+            df['direct_spectrum'] = Distribution(solar_spectrum['wavelength'][10:37],
+                                                 np.squeeze(solar_spectrum['poa_direct'][10:37]))
+            df['diffuse_spectrum'] = Distribution(solar_spectrum['wavelength'][10:37],
+                                                  np.squeeze(solar_spectrum['poa_sky_diffuse'][10:37]))
+        except ValueError:
+            # When the sun is behind the array the sign of AoI dot product becomes negative. We skip those time point ;)
+            return df
+
+        # Calculate irradiance from spectral results in the spectral range of interest for simulations (see note above)
+        df["direct_irradiance"] = integrate.trapz(df['direct_spectrum']._y, df['direct_spectrum']._x)
+        df["diffuse_irradiance"] = integrate.trapz(df['diffuse_spectrum']._y, df['diffuse_spectrum']._x)
 
         return df
 
@@ -83,18 +93,11 @@ def solar_data_for_place_and_time(site: Location, datetime_points: pd.core.index
 
     # Calculate spectra (diffuse and direct incident on reactor) per each data-time point
     solar_data = solar_data.apply(calculate_spectrum, axis=1)
-    print(solar_data.columns)
-
-
-    fig, (ax1, ax2) = plt.subplots(1,2)
-    ax1.scatter(solar_data['dni'], solar_data['dni_spctral'])
-    ax2.scatter(solar_data['dhi'], solar_data['dhi_spctral'])
-    plt.show()
-    plt.savefig("comparison_spectral_get_clearsky.png")
+    # print(solar_data.columns)
 
     # Export to CSV
     solar_data.to_csv(f"Full_data_{site.name}.csv", columns=('apparent_zenith', 'zenith', 'apparent_elevation', 'elevation',
-       'azimuth', 'airmass_relative', 'aoi', 'dni', 'dhi', 'dni_spctral', 'dhi_spctral'))
+       'azimuth', 'airmass_relative', 'aoi', 'direct_irradiance', 'diffuse_irradiance', 'direct_spectrum', 'diffuse_spectrum'))
 
     logger = logging.getLogger("pvtrace").getChild("miniplant")
     logger.info(f"Generated solar data for {site.name} [lat. {site.latitude}, long. {site.longitude}] in the time range"
@@ -106,10 +109,7 @@ def solar_data_for_place_and_time(site: Location, datetime_points: pd.core.index
 if __name__ == '__main__':
     from miniplant.locations import NORTH_CAPE
     # Time points to be calculated
-    year2020 = pd.date_range(start=datetime.datetime(2020, 3, 30), end=datetime.datetime(2020, 4, 1), freq='0.5H')
+    year2020 = pd.date_range(start=datetime.datetime(2020, 1, 1), end=datetime.datetime(2021, 1, 1), freq='0.5H')
 
     # perform calculations
     test_df = solar_data_for_place_and_time(NORTH_CAPE, year2020, 30)
-
-    print(test_df)
-
