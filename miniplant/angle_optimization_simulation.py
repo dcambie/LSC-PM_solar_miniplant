e4b9be3f5ee348404f7c8b7dd80260fa7b968b81
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-02-15 23:21:27 +0100

    Settings for new sim

--- miniplant/angle_optimization_simulation.py
+++ miniplant/angle_optimization_simulation.py
@@ -105,10 +105,10 @@ def evaluate_tilt_angle(
 
 
 if __name__ == "__main__":
-    from miniplant.locations import NORTH_CAPE
+    from miniplant.locations import TOWNSVILLE
 
-    site = NORTH_CAPE
+    site = TOWNSVILLE
 
-    tilt_range = list(range(40, 71, 5))
+    tilt_range = list(range(0, -40, -5))
     for tilt in tilt_range:
         evaluate_tilt_angle(tilt_angle=tilt, location=site, workers=12, time_resolution=1800)
