fb5fe7b7a6a564841a4af17d8963a1735191e5eb
Dario Cambié <dario.cambie@mpikg.mpg.de>
2021-02-19 10:04:55 +0100

    Update full_simulation_analysis.py

--- miniplant/full_simulation_results/full_simulation_analysis.py
+++ miniplant/full_simulation_results/full_simulation_analysis.py
@@ -14,10 +14,9 @@ GOLDEN_RATIO = (1 + 5 ** 0.5) / 2
 
 search_path = Path(".")
 result_files = list(search_path.rglob('*.csv'))
-result_files = [Path("./Eindhoven/Eindhoven_0deg_results.csv"), Path("./Eindhoven/Eindhoven_40deg_results.csv"),
-                Path("North Cape/North Cape_50deg_results_no_dye.csv"), Path(
-        "Townsville/Townsville_-10deg_results_no_dye.csv"),
-                Path("Plataforma Solar de Almería/Plataforma Solar de Almería_30deg_results_no_dye.csv")]
+result_files = [Path("North Cape/North Cape_50deg_results.csv"), Path("./Eindhoven/Eindhoven_0deg_results.csv"),
+                Path("./Eindhoven/Eindhoven_40deg_results.csv"),Path("Townsville/Townsville_-10deg_results.csv"),
+                Path("Plataforma Solar de Almería/Plataforma Solar de Almería_30deg_results.csv")]
 
 
 fig, ax = plt.subplots(ncols=len(result_files))
@@ -27,7 +26,7 @@ for ix, simulation_results_file in enumerate(result_files):
     # Extract location and angle from file name
     location_name = str(simulation_results_file.parent)
     tilt_angle = simulation_results_file.stem[len(location_name)+1:-11]
-    caption = f"{location_name} {tilt_angle}°"
+    caption = f"{location_name[:10]}... {tilt_angle}°" if len(location_name) > 13 else f"{location_name} {tilt_angle}°"
 
     # Load data
     df = pd.read_csv(simulation_results_file, parse_dates=[0], index_col=0, date_parser=lambda col: pd.to_datetime(col, utc=True))
@@ -43,22 +42,29 @@ for ix, simulation_results_file in enumerate(result_files):
         ax[ix].set_ylabel("Daily absorbed photon flux (mol/day)")
 
     # Set limits to prevent double January ;)
-    ax[ix].set_xlim([datetime(2020, 1, 1), datetime(2020, 12, 31)])
+    ax[ix].set_xlim([datetime(2020, 1, 1), datetime(2021, 1, 1)])
 
     # Date formatter to only show the month from the datetime object, and locator to show every month
     date_form = DateFormatter("%b")  # %b Show month names %m for month numbers
-    ax[ix].set_title(caption)
+    tot = daily["total_reacted"].sum()
+    ax[ix].set_title(caption+f"TOT: {tot:.0f}")
+
+    # ax[ix].suptitle = f"TOT: {tot}"
     if max(daily["total_reacted"])*1.05 > maxy:
         maxy = max(daily["total_reacted"])*1.05
     ax[ix].set_ylim(0, maxy)
     ax[ix].xaxis.set_major_formatter(date_form)
-    ax[ix].xaxis.set_major_locator(mdates.MonthLocator())
+    ax[ix].xaxis.set_major_locator(mdates.MonthLocator(interval=3))
 
     ax[ix].plot(daily.index, daily["total_reacted"])
     # Add legend
     ax[ix].fill_between(daily.index, 0, daily["direct_reacted"], alpha=0.5, label="direct irradiation")
     ax[ix].fill_between(daily.index, daily["direct_reacted"], daily["total_reacted"], alpha=0.5, label="diffuse irradiation")
     # ax[ix].legend(loc="upper right", ncol=1)
+
+    if ix == len(result_files)-1:
+        handles, labels = ax[ix].get_legend_handles_labels()
+        fig.legend(handles, labels, loc='lower center')
 
 fig.tight_layout()
 plt.show()
