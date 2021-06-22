bb77a954fa445caf4bfdb292aa6560c3f9c6b6b0
Dario Cambié <dario.cambie@glasgow.ac.uk>
2021-06-22 12:12:06 +0200

    Fix timerange of exp data

--- miniplant/experimental_comparison/simulate_experimental_conditions.py
+++ miniplant/experimental_comparison/simulate_experimental_conditions.py
@@ -10,8 +10,8 @@ logging.getLogger("pvtrace").setLevel(logging.WARNING)  # use logging.DEBUG for 
 
 from miniplant.locations import EINDHOVEN
 
-XP_START = datetime(2020, 7, 15, 15, 0, tzinfo=EINDHOVEN.pytz)  # 15th July 3pm
-XP_END = datetime(2020, 7, 15, 17, 0, tzinfo=EINDHOVEN.pytz)  # 15th July 5pm
+XP_START = EINDHOVEN.pytz.localize(datetime(2020, 7, 15, 15, 0))  # 15th July 3pm
+XP_END = EINDHOVEN.pytz.localize(datetime(2020, 7, 15, 17, 30))  # 15th July 5pm
 
 target_file = Path(f"./experimental_conditions_results.csv")
 
@@ -19,5 +19,8 @@ time_interval = (XP_START, XP_END)
 
 
 if __name__ == '__main__':
+    XP_START = datetime(2020, 7, 15, 15, 0, tzinfo=EINDHOVEN.pytz)  # 15th July 3pm
+    print(XP_END)
+    breakpoint()
     yearlong_simulation(tilt_angle=40, location=EINDHOVEN, workers=12, time_resolution=60 * 30, include_dye=True,
                         num_photons_per_simulation=10000, time_range=time_interval, target_file=target_file)
