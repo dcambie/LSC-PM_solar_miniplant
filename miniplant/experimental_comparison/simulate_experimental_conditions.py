07dae96583460a544c1d9af66596943c217ee2bb
Dario Cambié <dario.cambie@glasgow.ac.uk>
2021-06-22 11:39:25 +0200

    Little script to generate data to match exp. results

---
+++ miniplant/experimental_comparison/simulate_experimental_conditions.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timezone
+import logging
+from pathlib import Path
+
+from miniplant.full_simulation import yearlong_simulation
+
+logging.getLogger("trimesh").disabled = True
+logging.getLogger("shapely.geos").disabled = True
+logging.getLogger("pvtrace").setLevel(logging.WARNING)  # use logging.DEBUG for more printouts
+
+from miniplant.locations import EINDHOVEN
+
+xp_start = datetime(2020, 7, 15, 15, 0, tzinfo=EINDHOVEN.pytz)  # 15th July 3pm
+xp_end = datetime(2020, 7, 15, 17, 0, tzinfo=EINDHOVEN.pytz)  # 15th July 5pm
+
+target_file = Path(f"./experimental_conditions_results.csv")
+
+time_interval = (xp_start, xp_end)
+
+
+if __name__ == '__main__':
+    yearlong_simulation(tilt_angle=40, location=EINDHOVEN, workers=12, time_resolution=60 * 30, include_dye=True,
+                        num_photons_per_simulation=10000, time_range=time_interval, target_file=target_file)
