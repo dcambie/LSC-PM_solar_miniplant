b22227d867c6756bd73161359f840cc2d8ca98ea
Stefan Zondag <stefan-zondag@hotmail.com>
2022-01-06 17:23:45 +0100

    Implemented both conical and Lambertian LED in function. Added material appearance properties (functional) and the ability to vary between un-dyed coating, dyed coating and no coating. Added additional scene to enable LED location visualisation without slowing the ray-tracing. Fine-tuned LED positions. Using remote pvtrace branch all_PR_merged (local name?) all works fine.

--- miniplant/scene_creator.py
+++ miniplant/scene_creator.py
@@ -3,8 +3,7 @@ import io
 import logging
 from typing import Callable
 
-from pvtrace.material.utils import lambertian
-from pvtrace.material.utils import cone
+from pvtrace.material.utils import cone, lambertian
 
 import pandas as pd
 import numpy as np
@@ -26,13 +25,12 @@ from pvtrace import (
     Distribution,
 )
 from pvtrace import isotropic
-from pvtrace.material.utils import spherical_to_cart
 
 import pkgutil
 from miniplant import reactor_data
 
 MB_ABS_DATAFILE = pkgutil.get_data(__name__, "reactor_data/MB_1M_1m_ACN.tsv")
-LR305_ABS_DATAFILE = pkgutil.get_data(__name__, "reactor_data/Evonik_lr305_normalized_to_1m.tsv")
+LR305_ABS_DATAFILE = pkgutil.get_data(__name__, "reactor_data/Red_Absorption.txt")
 LR305_EMS_DATAFILE = pkgutil.get_data(__name__, "reactor_data/Evonik_lr305_normalized_to_1m_ems.tsv")
 WHITE_LED_EMS_DATAFILE = pkgutil.get_data(__name__, "reactor_data/White_LED_em.txt")
 
@@ -50,29 +48,70 @@ LTF_W = 60e-3
 LTF_L = 101e-3
 t_coating = 18e-6
 LTF_H = 6e-3
-
 # REACTOR_AREA_IN_M2 = LTF_W * LTF_L
 
-wire_frame = False
-
-
-def down_facing_LED():
-    """
-    Gets Lambertian direction and change Y axis
-    """
-    coord = lambertian()
-    return coord[0], coord[1], -coord[2]
-
-
+# LED emission data
 emission = pd.read_csv(io.BytesIO(WHITE_LED_EMS_DATAFILE), encoding="utf8", sep="\t").values
 dist = Distribution(x=emission[:, 0], y=emission[:, 1])
 
+# LED positions for the LED Box, in total 1170 LEDs.
+m = 39
+n = 30
+dx = 1.3e-2
+dy = 1.7e-2
+h_box = 29.8e-2
+LED_POSITION = [(-m / 2 * dx + (i + 0.5) * dx,
+                 -n / 2 * dy + (j + 0.5) * dy,
+                 h_box) for i in range(m) for j in range(n)]
 
-def wavelength_LED():
+
+def wavelength_led():
     return dist.sample(np.random.uniform())
 
 
-def _create_scene_common(light_source, include_dye=None) -> Scene:
+def led_pos():
+    return random.choice(LED_POSITION)
+
+
+def down_facing_led():
+    """
+    Gets Lambertian or conical direction and change Y axis
+    """
+    # coord = lambertian()
+    coord = cone(np.deg2rad(66))
+    return coord[0], coord[1], -coord[2]
+
+
+def create_led_scene() -> Scene:
+    world = Node(
+        name="World (air)",
+        geometry=Sphere(radius=1.0),
+    )
+
+    leds = []
+
+    # Create LEDs for visualisation
+    for led_num in range(len(LED_POSITION)):
+        leds.append(
+            Node(
+                name=f"LED_render_{led_num}",
+                geometry=Cylinder(
+                    length=0,
+                    radius=2.5e-3,
+                    material=None
+                ),
+                appearance=dict(color=0x000000,
+                                ),
+                parent=world,
+            )
+        )
+        # # Adjust capillary position
+        leds[-1].translate((LED_POSITION[led_num][0], LED_POSITION[led_num][1], LED_POSITION[led_num][2]))
+
+    return Scene(world)
+
+
+def _create_scene_common(light_source, include_coating=None, include_dye=None) -> Scene:
     logger = logging.getLogger("pvtrace").getChild("miniplant")
     logger.debug(f"Creating simulation scene.")
 
@@ -86,24 +125,19 @@ def _create_scene_common(light_source, include_dye=None) -> Scene:
     # Bind the light source to the current world
     light_source.parent = world
 
-    # Glass matrix
+    # Glass matrix and appearance
     glass_component = [Absorber(coefficient=0.1)]  # PMMA background absorption
+    glass_color = 0xFFFFFF
+    glass_vis_prop = dict(transparent=False,
+                          opacity=0.8,
+                          wireframe=True,
+                          )
+    channel_color = 0x0000FF
+    channel_vis_prop = dict(transparent=True,
+                            opacity=0.9,
+                            wireframe=True)
 
-    # Coating matrix
-    coating_component = [Absorber(coefficient=0.1)]  # PMMA background absorption
-
-    if include_dye is None:
-        include_dye = True
-
-    if include_dye:
-        coating_component.append(Luminophore(
-            coefficient=pd.read_csv(io.BytesIO(LR305_ABS_DATAFILE), encoding="utf8", sep="\t").values,
-            emission=pd.read_csv(io.BytesIO(LR305_EMS_DATAFILE), encoding="utf8", sep="\t").values,
-            quantum_yield=0.95,
-            phase_function=isotropic,
-        ))
-
-    # LSC object
+    # Glass reactor object
     reactor = Node(
         name="LTF Reactor",
         geometry=Box(
@@ -113,51 +147,17 @@ def _create_scene_common(light_source, include_dye=None) -> Scene:
                 components=glass_component,
             ),
         ),
-        appearance=dict(color=0xFFFFFF,
-                        transparent=True,
-                        opacity=1,
-                        wireframe=True),
+        appearance=dict(color=glass_color,
+                        ),
         parent=world,
     )
-
-    coating_vis_prop = dict(color=0xFF0000,
-                            transparent=True,
-                            opacity=0.5,
-                            wireframe=wire_frame, )
-
-    upper_coating = Node(
-        name="Top coating",
-        geometry=Box(
-            size=(LTF_W, LTF_L, t_coating),
-            material=Material(
-                refractive_index=Coating_RI,
-                components=coating_component,
-            ),
-        ),
-        appearance=coating_vis_prop,
-        parent=reactor,
-    )
-
-    lower_coating = Node(
-        name="Bottom coating",
-        geometry=Box(
-            size=(LTF_W, LTF_L, t_coating),
-            material=Material(
-                refractive_index=Coating_RI,
-                components=coating_component,
-            ),
-        ),
-        appearance=coating_vis_prop,
-        parent=reactor,
-    )
-
-    upper_coating.translate((0, 0, 1 / 2 * (LTF_H + t_coating)))
-    lower_coating.translate((0, 0, -1 / 2 * (LTF_H + t_coating)))
+    reactor.appearance["meshcat"] = glass_vis_prop
 
     # Reaction Mixture absorption
     reaction_absorption_coefficient = pd.read_csv(io.BytesIO(MB_ABS_DATAFILE), encoding="utf8", sep="\t").values
     reaction_mixture_material = Reactor(reaction_absorption_coefficient)
 
+    # Inserting reaction mixture channels in the glass reactor object
     ltf = Node(
         name='LTF channels',
         geometry=Mesh(
@@ -167,47 +167,94 @@ def _create_scene_common(light_source, include_dye=None) -> Scene:
                 components=[reaction_mixture_material]
             ),
         ),
-        appearance=dict(color=0x0000FF,
-                transparent=True,
-                opacity=0.8,
-                wireframe=True),
+        appearance=dict(color=channel_color,
+                        ),
         parent=reactor,
     )
-
+    ltf.appearance["meshcat"] = channel_vis_prop
     ltf.translate((0, -2.32400e-3, 0))
 
-    reactor.translate((0, 0, -1 / 2 * LTF_H - t_coating))
+    if include_coating is None:
+        include_coating = True
+
+    if include_dye is None:
+        include_dye = True
+
+    if include_dye is True and include_coating is False:
+        raise RuntimeError("Sorry, cannot enable dye without a coating!")
+
+    if include_coating:
+        t_coating_factor = 1
+        # Coating matrix and appearance
+        coating_component = [Absorber(coefficient=0.1)]  # PMMA background absorption
+        coating_color = 0xFFFFFF
+        coating_vis_prop = dict(transparent=True,
+                                opacity=0.3,
+                                wireframe=False,
+                                )
+        if include_dye:
+            coating_color = 0xFF0000
+            coating_component.append(Luminophore(
+                coefficient=pd.read_csv(io.BytesIO(LR305_ABS_DATAFILE), encoding="utf8", sep="\t").values,
+                emission=pd.read_csv(io.BytesIO(LR305_EMS_DATAFILE), encoding="utf8", sep="\t").values,
+                quantum_yield=0.95,
+                phase_function=isotropic,
+            ))
+
+        upper_coating = Node(
+            name="Top coating",
+            geometry=Box(
+                size=(LTF_W, LTF_L, t_coating),
+                material=Material(
+                    refractive_index=Coating_RI,
+                    components=coating_component,
+                ),
+            ),
+            appearance=dict(color=coating_color,
+                            ),
+            parent=reactor,
+        )
+        upper_coating.appearance["meshcat"] = coating_vis_prop
+
+        lower_coating = Node(
+            name="Bottom coating",
+            geometry=Box(
+                size=(LTF_W, LTF_L, t_coating),
+                material=Material(
+                    refractive_index=Coating_RI,
+                    components=coating_component,
+                ),
+            ),
+            appearance=dict(color=coating_color,
+                            ),
+            parent=reactor,
+        )
+        lower_coating.appearance["meshcat"] = coating_vis_prop
+
+        upper_coating.translate((0, 0, 1 / 2 * (LTF_H + t_coating)))
+        lower_coating.translate((0, 0, -1 / 2 * (LTF_H + t_coating)))
+    else:
+        t_coating_factor = 0
+
+    reactor.translate((0, 0, -1 / 2 * LTF_H - t_coating_factor*t_coating))
 
     return Scene(world)
 
 
 def create_direct_scene(
-        light_distribution: Callable = lambda: 555,
+        include_coating: bool = True,
         include_dye: bool = None
 ) -> Scene:
-    """ Create a scene with a fixed light position and direction, to match direct irradiation """
-
-    m = 39
-    n = 29
-    dy = 1.3e-2
-    dx = 1.7e-2
-    h_box = 29.8e-2
-    LED_POSITION = [(-np.ceil(n / 2) * dx + i * dx,
-                     -np.ceil(m / 2) * dy + j * dy,
-                     h_box) for i in range(n) for j in range(m)]
-
-    def LED_pos():
-        return random.choice(LED_POSITION)
-
+    """ Create a scene with fixed light positions and directions, to match LED Box irradiation """
     # Create light
-    LED_light = Node(
+    led_light = Node(
         name="Solar Light",
         light=Light(
-            wavelength=lambda: wavelength_LED(),
-            direction=down_facing_LED,
-            position=LED_pos,
+            wavelength=lambda: wavelength_led(),
+            direction=down_facing_led,
+            position=led_pos,
         ),
         parent=None,
     )
 
-    return _create_scene_common(light_source=LED_light, include_dye=include_dye)
+    return _create_scene_common(light_source=led_light, include_coating=include_coating, include_dye=include_dye)
--- miniplant/simulation_runner.py
+++ miniplant/simulation_runner.py
@@ -10,69 +10,61 @@ from typing import Callable
 
 from pvtrace import photon_tracer, MeshcatRenderer, Event, Scene
 
-from miniplant.scene_creator import create_direct_scene, emission
+from miniplant.scene_creator import create_direct_scene, create_led_scene, emission
 
 logger = logging.getLogger("pvtrace").getChild("miniplant")
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
 
 
 def _common_simulation_runner(
-    scene: Scene, num_photons: int = 100, render: bool = False, workers: int = None
+        scene: Scene, num_photons: int = 100, render: bool = False
 ):
     logger.debug(f"Starting ray-tracing with {num_photons} photons (Render is {render})")
 
-    if render and workers > 1:
-        raise RuntimeError("Sorry, cannot use renderer if more than 1 worker is used!")
+    if render:
+        renderer = MeshcatRenderer(open_browser=True,
+                                   opacity=None,
+                                   wireframe=None)
+        renderer.render(scene)
+    finals = []
+    led_wavelengths = []
+    valid_photon = 0
+    missed_photon = 0
 
-    # SINGLE-THREADED
-    if workers == 1:
-        if render:
-            renderer = MeshcatRenderer(open_browser=True)
-            renderer.render(scene)
-        finals = []
-        led_wavelengths = []
-        valid_photon = 0
-        missed_photon = 0
+    while True:
+        ray = next(scene.emit(1))
+        steps = photon_tracer.follow(scene, ray)
+        path, events = zip(*steps)
 
-        while True:
-            ray = next(scene.emit(1))
-            steps = photon_tracer.follow(scene, ray)
-            path, events = zip(*steps)
+        # Ensures only photons at least hitting the reactor are taken into account (events>2)
+        if len(events) > 2:
+            valid_photon += 1
+            # print(ray.wavelength)
+            finals.append(events[-1])
+            led_wavelengths.append(ray.wavelength)
+            if render:
+                renderer.add_ray_path(path)
+            if np.mod(100 * valid_photon / num_photons, 10) == 0:
+                print(f"Currently {100 * valid_photon / num_photons:.2f}% has been simulated")
+        else:
+            missed_photon += 1
 
+        if valid_photon >= num_photons:
+            if render:
+                # Now adding the LEDs for visualisation, but after simulating (due to speed)
+                new_scene = create_led_scene()
+                renderer.render(new_scene)
 
-            # Ensures only photons at least hitting the reactor are taken into account
-            if len(events) > 2:
-                valid_photon += 1
-                # print(ray.wavelength)
-                finals.append(events[-1])
-                led_wavelengths.append(ray.wavelength)
-                if render:
-                    renderer.add_ray_path(path)
-                if np.mod(100*valid_photon/num_photons, 10) == 0:
-                    print(f"Currently {100*valid_photon/num_photons:.2f}% has been simulated")
-            else:
-                missed_photon += 1
-
-            if valid_photon >= num_photons:
-                plt.hist(led_wavelengths, bins=100, density=True, histtype='step',
-                         label='sample')
-                x = emission[:, 0]
-                y = emission[:, 1]
-                plt.plot(x, y / np.trapz(y, x), label='distribution')
-                plt.legend()
-                plt.xlabel("Wavelength (nm)")
-                plt.grid(linestyle='dotted')
-                plt.show()
-                break
-
-    else:
-        if render:
-            logger.warning(
-                "Cannot use renderer in multi-threaded simulations! Set workers to 1 for single-theaded"
-            )
-        # MULTI-THREADED
-        results = scene.simulate(num_rays=num_photons, workers=workers)
-        finals = [photon[-1][1] for photon in results]
+            plt.hist(led_wavelengths, bins=100, density=True, histtype='step',
+                     label='sample')
+            x = emission[:, 0]
+            y = emission[:, 1]
+            plt.plot(x, y / np.trapz(y, x), label='distribution')
+            plt.legend()
+            plt.xlabel("Wavelength (nm)")
+            plt.grid(linestyle='dotted')
+            plt.show()
+            break
 
     count_events = collections.Counter(finals)
     reacted_fraction = count_events[Event.REACT] / valid_photon
@@ -97,19 +89,18 @@ def _common_simulation_runner(
 
 
 def run_direct_simulation(
-    light_distribution: Callable = lambda: 555,
-    num_photons: int = 100,
-    render: bool = False,
-    workers: int = None,
-    include_dye: bool = None
+        num_photons: int = 100,
+        render: bool = False,
+        include_coating: bool = True,
+        include_dye: bool = True
 ) -> float:
     """
     Create a scene for direct irradiation with the provided parameters and runs a simulation on it
     """
-    scene = create_direct_scene(include_dye=include_dye)
-    return _common_simulation_runner(scene, num_photons, render, workers)
+    scene = create_direct_scene(include_coating=include_coating, include_dye=include_dye)
+    return _common_simulation_runner(scene, num_photons, render)
 
 
 if __name__ == "__main__":
-    run_direct_simulation(num_photons=10000, render=True, workers=1, include_dye=False)
+    run_direct_simulation(num_photons=100, render=True, include_coating=True, include_dye=True)
     input()
